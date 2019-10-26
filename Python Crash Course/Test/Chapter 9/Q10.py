f07e18043f1c360b3f0a330193acb699f0e73825
YanliangZhou <yzmz1314@users.noreply.github.com>
2019-10-26 02:07:41 -0500

    new test

--- Python Crash Course/Test/Chapter 9/Q10.py
+++ Python Crash Course/Test/Chapter 9/Q10.py
@@ -1,6 +1,6 @@
 # 9-10 导入Restaurant 类 ：将最新的Restaurant 类存储在一个模块中。在另一个文件中，导入Restaurant 类，创建一个Restaurant
 # 实例，并调用Restaurant 的一个方法，以确认import 语句正确无误。
-from 10_1 import Restaurant
+from Q10_1 import Restaurant
 
 restaurant = Restaurant("Elf kitchen", "Chinese")
 restaurant.discribe_restaurant()
--- Python Crash Course/Test/Chapter 9/Q11.py
+++ Python Crash Course/Test/Chapter 9/Q11.py
@@ -1,2 +1,6 @@
 # 9-11 导入Admin 类 ：以为完成练习9-8而做的工作为基础，将User 、Privileges 和Admin 类存储在一个模块中，再创建一个文件，
 # 在其中创建一个Admin 实例并对其调用方法show_privileges() ，以确认一切都能正确地运行。
+import Q11_1
+
+user = Q11_1.Admin("Yanliang", "Zhou")
+user.privilege.show_privileges()
