eeff18f0c6240cf118b4dd16389fde36c5fe4704
YanliangZhou <yzmz1314@users.noreply.github.com>
2019-10-26 03:44:02 -0500

    new test

--- Python Crash Course/Test/Chapter 9/q12.py
+++ Python Crash Course/Test/Chapter 9/q12.py
@@ -1,6 +1,6 @@
 # 9-12 多个模块 ：将User 类存储在一个模块中，并将Privileges 和Admin 类存储在另一个模块中。再创建一个文件，在其中创建一个
 # Admin 实例，并对其调用方法show_privileges() ，以确认一切都依然能够正确地运行。
-from Q12_2 import Privileges, Admin
+from q12_2 import Privileges, Admin
 
 user = Admin("Yanliang", "Zhou")
 user.privilege.show_privileges()
