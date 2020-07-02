2f9c35bcc90657db349dce7c087b0509fcb70b86
yanliangzhou <yzmz1314@gmail.com>
2020-07-01 23:15:22 -0500

    0701

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,15 +1,44 @@
-class Solution:
-    def generate(self, numRows):
-        if numRows > 0:
-            res = [[1]]
-            for i in range(1, numRows):
-                res += [list(map(lambda x, y: x + y, res[-1] + [0], [0] + res[-1]))]
-            return res
-        else:
-            return []
-# 需要考虑 numRows = 0的情况
-if __name__ == "__main__":
-    gen = Solution()
-    Gen = gen.generate(5)
-for i in range(len(Gen)):
-    print(Gen[i])
+# # for i in range(10):
+# #     exec('a%s=%d'%(i, i))
+# # for i in range(10):
+# #     exec('print(a%s)'%i)
+#
+# import numpy as np
+#
+#
+# def fun(n, in_list, state, out_list):
+#     num = n
+#     if (n == 0):
+#         print(out_list)
+#     else:
+#         for i in range(len(in_list)):
+#             if state[i]:
+#                 out_list[n - 1] = in_list[i]
+#                 state[i] = False
+#                 fun(n - 1, in_list, state, out_list)
+#                 state[i] = True
+#
+#
+# def full_permutation(in_list):
+#     n = len(in_list)
+#     state = [True for _ in range(n)]
+#     out_list = in_list.copy()
+#     fun(n, in_list, state, out_list)
+#
+#
+# a = np.arange(3).tolist()
+# full_permutation(a)
+#
+# # if __name__ == "__main__":
+# #     l = [1,2,3]
+# #     result = full_permutation(l)
+# #     # print(result)
+def fact(n):
+    return fact_iter(n, 1)
+
+def fact_iter(num, product):
+    if num == 1:
+        return product
+    return fact_iter(num - 1, num * product)
+m = fact(999)
+print(m)
