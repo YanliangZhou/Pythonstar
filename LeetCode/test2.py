728981472e4235a135d0fe0a2282f76cf78f993a
yanliangzhou <yzmz1314@gmail.com>
2020-07-06 02:57:29 -0500

    0706

--- LeetCode/test2.py
+++ LeetCode/test2.py
@@ -1,8 +1,7 @@
-def fact(n):
-    return fact_iter(n, 1)
-
-def fact_iter(num, product):
-    if num == 1:
-        return product
-    return fact_iter(num - 1, num * product)
-fact(1000)
+def add_end(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+print(add_end())
+print(add_end())
