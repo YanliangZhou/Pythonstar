d66b4f20f1bae51f1298c5747c3bf101160e5672
yanliangzhou <yzmz1314@gmail.com>
2020-08-18 20:54:49 -0500

    08/18/2020

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,2 +1,21 @@
-ex = {}
-print(type(ex))
+class Solution:
+    def twoSum(self, nums, target):
+       diction = {}
+       count = 0
+       for i in range(len(nums)):
+           if nums[i] in diction.keys() and 2*nums[i] == target:
+               return [diction[nums[i]], count]
+           diction[nums[i]] = i
+           remain = target - nums[i]
+           count += 1
+           if remain in diction.keys() and diction[remain] != diction[nums[i]]:
+               if diction[remain] < diction[nums[i]]:
+                    return [diction[remain], i]
+               else:
+                    return [i, diction[remain]]
+
+if __name__ == "__main__":
+    list = [3, 3]
+    answer = Solution()
+    sol = answer.twoSum(list, 6)
+    print(sol)
