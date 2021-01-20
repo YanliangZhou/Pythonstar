b7b1223629081052952a20c1d137c3b2de598de1
YanliangZhou <yzmz1314@users.noreply.github.com>
2021-01-19 18:41:38 -0600

    data

--- djangoProject/mgr/urls.py
+++ djangoProject/mgr/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', views.listorders,name="home"),
     path('orders', listorders),
     # path('customers', customer.dispatcher),
-    # path('medicines', medicine.dispatcher),
+    # path('medicines', medicine.dispat cher),
     path('signin', sign_in_out.signin),
     path('signout', sign_in_out.signout),
 ]
